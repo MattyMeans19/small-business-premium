f6a84b840a3a268874ba81af630ac708343558ae
Matt <mattymeans@gmail.com>
2025-08-14 23:09:49 -0600

    Debugging path

--- Backend/index.js
+++ Backend/index.js
@@ -461,7 +461,7 @@ db.connect()
   //        }
   //});
 
-const buildPath = path.join(__dirname, '..', 'FrontEnd', 'build');
+const buildPath = path.resolve(__dirname, '..', 'FrontEnd', 'build');
 
 app.use(express.static(buildPath));
 
