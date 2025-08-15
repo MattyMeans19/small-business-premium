a0746ad657283bcffde7cbfbbdb87cd9f8ceba73
Matt <mattymeans@gmail.com>
2025-08-14 22:10:25 -0600

    Fixed ESM syntax

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import pg from 'pg';
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
